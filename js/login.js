a2a3cd7d747a14e0c1bed8bd52014341270bbe6f
strongminmin <15036875971@163.com>
2018-11-16 19:43:34 +0800

    添加 登录页面的验证信息

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+// 视图切换
 const SwitchView = {
     init(){
         this.render();
@@ -43,7 +44,7 @@ const SwitchView = {
 }
 SwitchView.init();
 
-
+// 登录模块
 const Login = {
     init(){
         this.render();
@@ -51,46 +52,63 @@ const Login = {
     },
     render(){
         this.loginUname = $('#username');
-        this.loginTips = $("#login-uname-tips");
+        this.nameTip = $("#login-uname-tips");
+        this.passTip = $("#login-pass-tips");
         this.signinSubmit = $("#signin-btn");
         this.password = $("#password");
+        this.nameDrag = false;
+        this.passDrag = false;
     },
     bindEvent(){
-        this.loginUname.on('input',debounce(this.methods.isNameRule.bind(this),1000));
-        this.loginUname.on('focus',this.methods.errTip.bind(this));
+        this.loginUname.on('input',debounce(this.methods.isNameRule.bind(this),500));
+        this.password.on('input',debounce(this.methods.isPassRule.bind(this),500));
         this.signinSubmit.on('click',throttle(this.methods.ajaxSubmit.bind(this),2000));
     },
     methods : {
         isNameRule : function(e){
+            this.nameDrag = false;
             var firstDrge = e.target.value.match(/^[A-z]/);
             var lengthDrag = e.target.value.match(/(\w){6,}/)
             if (firstDrge && lengthDrag) {
-            this.loginTips.css("display", 'none');
+                this.nameDrag = true;
+                this.nameTip.removeClass("err-tips").html("用户名格式正确");
             } else {
-                this.loginTips.css("display",'block');
+                this.nameTip.html("用户名格式不正确").addClass("err-tips").css("display", "block");
             }
         },
-        errTip : function(){
-            this.loginTips.css("display", 'none');             
-        },
+         isPassRule: function (e) {
+             this.passDrag = false;
+             var drag = e.target.value.match(/[A-z0-9_!\.@+-]{6,}/) != null ? true : false;
+             if (drag) {
+                 this.passDrag = true;
+                 this.passTip.removeClass("err-tips").html("密码格式正确");
+             } else {
+                 this.passTip.addClass("err-tips").html("密码格式不正确").css("display", "block");
+             }
+         },
         ajaxSubmit : function(){
-            var data = `name=${this.loginUname.val()}&password=${this.password.val()}`;
-            console.log(data);
-            const promise = asyncMethods.postMessages("http://jsonplaceholder.typicode.com/posts");
-            promise.then(data => {
-                new Tips("success",'登录成功',2000);
-                setTimeout(()=> {
-                     window.location.href = `index/${data.id}`;
-                },1000)
-            }).catch(err => {
-                new Tips('error','账号或密码输入错误',2000);
-            })
+           if(this.nameDrag&&this.passDrag) {
+                var data = `name=${this.loginUname.val()}&password=${this.password.val()}`;
+                console.log(data);
+                const promise = asyncMethods.postMessages("http://jsonplaceholder.typicode.com/posts");
+                promise.then(data => {
+                    new Tips("success", '登录成功', 2000);
+                    setTimeout(() => {
+                        window.location.href = `index/${data.id}`;
+                    }, 1000)
+                }).catch(err => {
+                    new Tips('error', '用户名或密码输入错误', 2000);
+                })
+           } else {
+               new Tips('error','用户名或密码输入错误',1000);
+           }
         },
 
     },
 }
  Login.init();
 
+//找回密码模块
  const Retrieve = {
      init(){
          this.render();
@@ -101,38 +119,59 @@ const Login = {
          this.retrieveTips = $("#retrieve-tips");
          this.newPassword = $("#new-password");
          this.retrieveBtn = $("#retrieve-btn");
+         this.passTip = $("#retrieve-pass-tips")
+         this.emialDrag = false;
+         this.passDrag = false;
      },
      bindEvent(){
          this.retrieveEmial.on("input",debounce(this.methods.isEmialRule.bind(this),500));
+         this.newPassword.on('input',debounce(this.methods.isPassRule.bind(this),500));
          this.retrieveBtn.on("click",this.methods.ajaxSubmit.bind(this));
      },
      methods:{
          isEmialRule : function(e){
+             this.emialDrag =false;
              var drag = e.target.value.match(/^([w]{3}\.|[w]{0})([\w\W]{1,})(\.com$)/) != null ? true : false;
              if(drag){
+                 this.emialDrag = true;
                  this.retrieveTips.removeClass("err-tips").html("邮箱格式正确");
              } else {
                  this.retrieveTips.addClass("err-tips").html("邮箱格式不正确").css("display","block");
              }
          },
+         isPassRule : function(e){
+            this.passDrag = false;
+            var drag = e.target.value.match(/[A-z0-9_!\.@+-]{6,}/) != null ? true : false;
+            if(drag){
+                this.passDrag = true;
+                this.passTip.removeClass("err-tips").html("密码格式正确");
+            } else {
+                this.passTip.addClass("err-tips").html("密码格式不正确").css("display", "block");
+            }
+         },
          ajaxSubmit : function(){
-             var data = `emial=${this.retrieveEmial.val()}&newPassword=${this.newPassword.val()}`
-             console.log(data);
-             const promise = asyncMethods.postMessages("http://jsonplaceholder.typicode.com/posts",data);
-             promise.then(data => {
-                 new Tips("success","修改成功",1000);
+             if(this.emialDrag&&this.passDrag) {
+                 var data = `emial=${this.retrieveEmial.val()}&newPassword=${this.newPassword.val()}`
                  console.log(data);
-                 setTimeout(()=> {
-                    location.reload();
-                 },500)
-             }).catch(err => {
-                 new Tips("error","修改失败",1000);
-             })
+                 const promise = asyncMethods.postMessages("http://jsonplaceholder.typicode.com/posts", data);
+                 promise.then(data => {
+                     new Tips("success", "修改成功", 1000);
+                     console.log(data);
+                     setTimeout(() => {
+                         location.reload();
+                     }, 500)
+                 }).catch(err => {
+                     new Tips("error", "修改失败", 1000);
+                 })
+             } else{
+                 new Tips("error","您输入的信息不正确",1000);
+             }
          }
      }
  }
  Retrieve.init();
 
+//注册模块
 const Register = {
     init(){
         this.render();
@@ -142,17 +181,68 @@ const Register = {
         this.registerName = $("#register-name");
         this.registerEmial = $("#register-emial");
         this.registerPassword = $("#register-password");
-        this.nameTips = $("#register-name-tips");
-        this.emialTips = $("#register-emial-tips");
-        this.passTips = $("#register-pass-tips");
+        this.nameTip = $("#register-name-tips");
+        this.emialTip = $("#register-emial-tips");
+        this.passTip = $("#register-pass-tips");
         this.registerBtn = $("#register-btn");
-        this.submitDrag = false;
+        this.nameDrag = false;
+        this.emialDrag = false;
+        this.passDrag = false;
     },
     bindEvent(){
-        
+        this.registerName.on('input',debounce(this.methods.isNameRule.bind(this),500));
+        this.registerEmial.on("input",debounce(this.methods.isEmialRule.bind(this),500));
+        this.registerPassword.on('input',debounce(this.methods.isPassRule.bind(this),500));
+        this.registerBtn.on("click",this.methods.ajaxSubmit.bind(this));
     },
     methods : {
-
+        isNameRule : function(e){
+            this.nameDrag =false;
+            var firstDrge = e.target.value.match(/^[A-z]/);
+            var lengthDrag = e.target.value.match(/(\w){6,}/)
+            if (firstDrge && lengthDrag) {
+                this.nameDrag = true;
+                this.nameTip.removeClass("err-tips").html("用户名格式正确");
+            } else {
+                this.nameTip.html("用户名格式不正确").addClass("err-tips").css("display","block");
+            }
+        },
+        isEmialRule : function(e){
+            this.emialDrag = false;
+            var drag = e.target.value.match(/^([w]{3}\.|[w]{0})([\w\W]{1,})(\.com$)/) != null ? true : false;
+            if (drag) {
+                this.emialDrag = true;
+                this.emialTip.removeClass("err-tips").html("邮箱格式正确");
+            } else {
+                this.emialTip.addClass("err-tips").html("邮箱格式不正确").css("display", "block");
+            }
+        },
+        isPassRule : function(e){
+            this.passDrag = false;
+            var drag = e.target.value.match(/[A-z0-9_!\.@+-]{6,}/) != null ? true : false;
+            if(drag){
+                this.passDrag = true;
+                this.passTip.removeClass("err-tips").html("密码格式正确");
+            } else {
+                this.passTip.addClass("err-tips").html("密码格式不正确").css("display", "block");
+            }
+        },
+        ajaxSubmit : function(){
+            if(this.nameDrag&&this.emialDrag&&this.passDrag){
+                var data = `name=${this.registerName.val()}&emial=${this.registerEmial.val()}&password=${this.registerPassword.val()}`;
+                const promise = asyncMethods.postMessages("http://jsonplaceholder.typicode.com/posts",data);
+                promise.then(data => {
+                    new Tips("success","注册成功",1000);
+                    setTimeout(()=> {
+                        location.reload();
+                    },500)
+                }).catch(err => {
+                    new Tips("error","注册失败",1000);
+                })
+            } else {
+                new Tips("error","您的信息输入不正确",1000);
+            }
+        }
     }
 }
 Register.init();
